c288e7328ba5984d03c56b5f33622b290271633c
radui1398 <48224025+radui1398@users.noreply.github.com>
2019-11-12 11:23:55 +0200

    Adaugare suport culoare mobile

--- templates/parts/page-head.php
+++ templates/parts/page-head.php
@@ -8,6 +8,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="theme-color" content="#db5945">
     <title><?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
