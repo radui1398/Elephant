64bc4639aaf63eabbaef3f7cb77aa8c0a3a9443e
radui1398 <48224025+radui1398@users.noreply.github.com>
2019-11-21 14:31:53 +0200

    update manager

--- framework/updater.bat
+++ framework/updater.bat
@@ -1,1 +1,1 @@
-curl -o controllers/Menu2.php https://github.com/radui1398/Elephant/tree/master/controllers/Menu.php
+curl -o controllers/Menu5.php https://github.com/radui1398/Elephant/tree/master/controllers/Menu.php
