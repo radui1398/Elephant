3f593769cc2f0e348fcd2f3a055bc6e9a048f63c
radui1398 <48224025+radui1398@users.noreply.github.com>
2019-11-21 14:40:03 +0200

    update manager

--- framework/update/update1.bat
+++ framework/update/update1.bat
@@ -1,1 +1,1 @@
-curl -o framework/update/update1.bat https://raw.githubusercontent.com/radui1398/Elephant/master/framework/updater.bat
+curl -o framework/update/update2.bat https://raw.githubusercontent.com/radui1398/Elephant/master/framework/update/update2.bat
