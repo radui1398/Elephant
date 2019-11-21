8feb771443d10a49062078d131c47bb7c0c9070e
radui1398 <48224025+radui1398@users.noreply.github.com>
2019-11-21 17:08:16 +0200

    BUGFIX

--- framework/update/update2.bat
+++ framework/update/update2.bat
@@ -3,7 +3,7 @@ curl -o controllers/Gallery.php https://raw.githubusercontent.com/radui1398/Elep
 curl -o controllers/Image.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Image.php
 curl -o controllers/Menu.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Menu.php
 curl -o controllers/Theme.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Theme.php
-curl -o controllers/Theme.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Repeater.php
+curl -o controllers/Repeater.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Repeater.php
 
 curl -o framework/config.php https://raw.githubusercontent.com/radui1398/Elephant/master/framework/config.php
 curl -o framework/util.php https://raw.githubusercontent.com/radui1398/Elephant/master/framework/util.php
