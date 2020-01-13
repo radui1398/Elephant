6dbc50ee0a0db50b8cec44db23d29d3481f62f84
Radu Ionut <radui1398@gmail.com>
2020-01-13 11:40:32 +0200

    Cache Control

--- framework/update/update1.bat
+++ framework/update/update1.bat
@@ -1,1 +1,1 @@
-curl -o framework/update/update2.bat https://raw.githubusercontent.com/radui1398/Elephant/master/framework/update/update2.bat
+curl -H 'Cache-Control: no-cache' -o framework/update/update2.bat https://raw.githubusercontent.com/radui1398/Elephant/master/framework/update/update2.bat
--- framework/update/update2.bat
+++ framework/update/update2.bat
@@ -1,12 +1,12 @@
-curl -o controllers/Enqueue.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Enqueue.php
-curl -o controllers/Gallery.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Gallery.php
-curl -o controllers/Image.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Image.php
-curl -o controllers/Menu.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Menu.php
-curl -o controllers/Theme.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Theme.php
-curl -o controllers/Repeater.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Repeater.php
+curl -H 'Cache-Control: no-cache' -o controllers/Enqueue.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Enqueue.php
+curl -H 'Cache-Control: no-cache' -o controllers/Gallery.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Gallery.php
+curl -H 'Cache-Control: no-cache' -o controllers/Image.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Image.php
+curl -H 'Cache-Control: no-cache' -o controllers/Menu.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Menu.php
+curl -H 'Cache-Control: no-cache' -o controllers/Theme.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Theme.php
+curl -H 'Cache-Control: no-cache' -o controllers/Repeater.php https://raw.githubusercontent.com/radui1398/Elephant/master/controllers/Repeater.php
 
-curl -o framework/config.php https://raw.githubusercontent.com/radui1398/Elephant/master/framework/config.php
-curl -o framework/util.php https://raw.githubusercontent.com/radui1398/Elephant/master/framework/util.php
-curl -o framework/security.php https://raw.githubusercontent.com/radui1398/Elephant/master/framework/security.php
+curl -H 'Cache-Control: no-cache' -o framework/config.php https://raw.githubusercontent.com/radui1398/Elephant/master/framework/config.php
+curl -H 'Cache-Control: no-cache' -o framework/util.php https://raw.githubusercontent.com/radui1398/Elephant/master/framework/util.php
+curl -H 'Cache-Control: no-cache' -o framework/security.php https://raw.githubusercontent.com/radui1398/Elephant/master/framework/security.php
 
-curl -o README.md https://raw.githubusercontent.com/radui1398/Elephant/master/README.md
+curl -H 'Cache-Control: no-cache' -o README.md https://raw.githubusercontent.com/radui1398/Elephant/master/README.md
